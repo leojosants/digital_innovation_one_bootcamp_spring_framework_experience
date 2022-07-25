84044404682d0730710bdc79e4ada69a680f4d8c
Leonardo Santos <leonardojosantos@gmail.com>
2022-07-24 22:17:20 -0300

    Testes relacionados ao Design Pattern Facade

--- Desafio_de_projeto_Explorando_Padroes_de_Projetos_na_pratica_com_Java/lab-padroes-projeto-java/src/Test.java
+++ Desafio_de_projeto_Explorando_Padroes_de_Projetos_na_pratica_com_Java/lab-padroes-projeto-java/src/Test.java
@@ -1,12 +1,4 @@
-
-import one.digitalinnovation.gof.singleton.SingletonEager;
-import one.digitalinnovation.gof.singleton.SingletonLazy;
-import one.digitalinnovation.gof.singleton.SingletonLazyHolder;
-import one.digitalinnovation.gof.strategy.Comportamento;
-import one.digitalinnovation.gof.strategy.ComportamentoAgressivo;
-import one.digitalinnovation.gof.strategy.ComportamentoDefensivo;
-import one.digitalinnovation.gof.strategy.ComportamentoNormal;
-import one.digitalinnovation.gof.strategy.Robo;
+import one.digitalinnovation.gof.facade.Facade;
 
 public class Test {
 
@@ -31,23 +23,30 @@ public class Test {
 //		lazyHolder = SingletonLazyHolder.getInstancia();
 //		System.out.println(lazyHolder);
 
+		
 		// Testes relacionados ao Design Pattern Strategy
 
-		Comportamento normal = new ComportamentoNormal();
-		Comportamento defensivo = new ComportamentoDefensivo();
-		Comportamento agressivo = new ComportamentoAgressivo();
+//		Comportamento normal = new ComportamentoNormal();
+//		Comportamento defensivo = new ComportamentoDefensivo();
+//		Comportamento agressivo = new ComportamentoAgressivo();
+//
+//		Robo robo = new Robo();
+//
+//		robo.setComportamento(normal);
+//		robo.mover();
+//
+//		robo.setComportamento(defensivo);
+//		robo.mover();
+//
+//		robo.setComportamento(agressivo);
+//		robo.mover();
 
-		Robo robo = new Robo();
-
-		robo.setComportamento(normal);
-		robo.mover();
-
-		robo.setComportamento(defensivo);
-		robo.mover();
-
-		robo.setComportamento(agressivo);
-		robo.mover();
-
+		
+		// Testes relacionados ao Design Pattern Facade
+		
+		Facade facade = new Facade();
+		facade.migrarCliente("Leonardo", "11.111-111");
+		
 	}
 
 }
