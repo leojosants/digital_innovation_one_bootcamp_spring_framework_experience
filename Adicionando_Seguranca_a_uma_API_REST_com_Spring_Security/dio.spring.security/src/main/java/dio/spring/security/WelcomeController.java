e9735a8512272e0d01ebfee9b7ff4a0df712fc38
Leonardo Santos <leonardojosantos@gmail.com>
2022-07-25 16:02:39 -0300

    Habilitando segurança com Spring e Autenticação simples

---
+++ Adicionando_Seguranca_a_uma_API_REST_com_Spring_Security/dio.spring.security/src/main/java/dio/spring/security/WelcomeController.java
@@ -0,0 +1,28 @@
+package dio.spring.security;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping
+    public  String welcome() {
+        return "Welcome to My Spring Boot Web API";
+    }
+
+    @GetMapping("/users")
+    @PreAuthorize("hasAnyRole('MANAGERS', 'USERS')")
+    public  String users() {
+        return "Authorized user";
+    }
+
+    @GetMapping("/managers")
+    @PreAuthorize("hasAnyRole('MANAGERS')")
+    public  String managers() {
+        return "Authorized managers";
+    }
+
+
+}
