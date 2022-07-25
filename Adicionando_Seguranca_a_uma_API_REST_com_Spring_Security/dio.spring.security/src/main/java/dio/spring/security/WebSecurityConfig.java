7dacfb1a1d164d2c04d918b0f08a01c07c61f358
Leonardo Santos <leonardojosantos@gmail.com>
2022-07-25 16:19:02 -0300

    Configure Adapter

--- Adicionando_Seguranca_a_uma_API_REST_com_Spring_Security/dio.spring.security/src/main/java/dio/spring/security/WebSecurityConfig.java
+++ Adicionando_Seguranca_a_uma_API_REST_com_Spring_Security/dio.spring.security/src/main/java/dio/spring/security/WebSecurityConfig.java
@@ -2,10 +2,12 @@ package dio.spring.security;
 
 import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.SecurityBuilder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -15,6 +17,15 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers(HttpMethod.POST, "/login").permitAll()
+                .antMatchers("/managers").hasAnyRole("MANAGERS")
+                .antMatchers("/users").hasAnyRole("USERS", "MANAGERS")
+                .anyRequest().authenticated().and().formLogin();
+    }
     @Override
     protected  void configure(AuthenticationManagerBuilder auth) throws  Exception {
         auth.inMemoryAuthentication()
