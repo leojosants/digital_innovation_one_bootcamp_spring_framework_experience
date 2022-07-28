949eaca646351d9d160d4aca08a44aa973d614cc
Leonardo Santos <leonardojosantos@gmail.com>
2022-07-28 03:03:26 -0300

    Apresentando a estrutura do pom.xml e as dependências do projeto

--- Realizando_Deploy_na_Nuvem_de_um_conjunto_de_APIs-desenvolvida_em_Spring_Boot/cloud-parking/src/main/java/com/digitalinnovationone/parking/CloudParkingApplication.java
+++ Realizando_Deploy_na_Nuvem_de_um_conjunto_de_APIs-desenvolvida_em_Spring_Boot/cloud-parking/src/main/java/com/digitalinnovationone/parking/CloudParkingApplication.java
@@ -2,6 +2,7 @@ package com.digitalinnovationone.parking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class CloudParkingApplication {
